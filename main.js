36dccd11066e7bdc9e9f55e520c98fddbdbd1538
sabit <asdsadasd>
2022-02-28 14:24:41 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var Name = prompt('Введите ваше имя?')
+var myAge = +prompt('Введите ваш возраст?')
+var consle = +prompt('Для входа в сервер нажмите "OK"')
+var exemple1 = +prompt('Решите пример: 6 + 6 = ?')
+var exemple2 = +prompt('Решите пример: 555 - 444 = ?')
+var exemple3 = +prompt('Решите пример: 10 * 10 = ?')
+var exemple4 = +prompt('Решите пример: 80 / 4 = ?')
+var exemple5 = +prompt('Решите пример: 16 % 3 = ?')
+var names = prompt('вы зошли в консоль "SABIT"')
+
+console.log(Name);
+console.log(myAge);
+console.log(consle);
+console.log(exemple1);
+console.log(exemple2);
+console.log(exemple3);
+console.log(exemple4);
+console.log(exemple5);
+console.log(names);
